c45c27490e98d6c46047d9e832e197d4eb922d54
Deepak NA <deepak.na@publicissapient.com>
2024-09-05 17:41:04 +0530

    first commit

---
+++ src/main/java/com/eca/eventManagement/EventManagementApplication.java
@@ -0,0 +1,13 @@
+package com.eca.eventManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+
+}
